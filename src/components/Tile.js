f4cf1fe61bc74b5bd13b6be87aa9b5386c884d14
josegibson <josegibson7@gmail.com>
2024-10-30 20:06:47 +0530

    url update

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Tile({ value, onClick }) {
     const getImageSource = () => {
-        const src = value === "X" ? "/images/x-image.png" : "/images/o-image.png";
+        const src = value === "X" ? `${process.env.PUBLIC_URL}/images/x-image.png` : `${process.env.PUBLIC_URL}/images/o-image.png`;
         console.log("Image source:", src); // Debugging line
         return src;
     };
